9adc371e5dd1ceb7a7d80979297604ddfa40c08c
Nadav <nadavops@protonmail.com>
2019-10-09 16:16:11 +0300

    modifications

--- AWS/WAP_ADFS_and_More/ms_scripts/ps_var_file.ps1
+++ AWS/WAP_ADFS_and_More/ms_scripts/ps_var_file.ps1
@@ -7,7 +7,7 @@ $AD_DS_Safe_Mode_Password = "Rr123456"
 $local_admin_name = "AppAdmin"
 $local_admin_password = "Aa123456"
 
-$domain_admin = "AppAdmin"
+$domain_admin_name = "AppAdmin"
 $domain_admin_password = "Dd123456"
 
 $user_password = "Uu123456"
