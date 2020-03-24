fd7398ce316f7ce8e7803b3902aa8f50de7b8cc8
Nadav Dola <nadav@Nadavs-MacBook-Pro.local>
2020-03-24 13:19:48 +0200

    some azure/ exchange scripts

---
+++ user_manipulation_in_azure_and_exchange/1.set_required_parameters.ps1
@@ -0,0 +1,11 @@
+$PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+$PasswordProfile.Password = ""
+
+$DisplayName = ""
+$Department = ""
+$DomainName = ""
+$JobTitle = ""
+
+$UsageLocation = ""
+
+$TemplateUser = ""
---
+++ user_manipulation_in_azure_and_exchange/5.add_user_to_office_groups.ps1
@@ -0,0 +1,6 @@
+# Finding the groups of the "template users"- using MS Msol service
+$Groups_list = foreach ($Group in (Get-MsolGroup -all)) {if (Get-MsolGroupMember -all -GroupObjectId $Group.ObjectId | where {$_.Emailaddress -eq $TemplateUser}) {$Group.DisplayName}}
+
+#Commands: (the first for security and distribution groups, the second for 365 groups)
+Foreach ($Group in $Groups_List) {Add-DistributionGroupMember -Identity $Group -Member "$FirstNameLowerCase@$DomainName.com"}
+Foreach ($Group in $Groups_List) {Add-UnifiedGroupLinks -Identity $group -LinkType Members -Links "$FirstNameLowerCase@$DomainName.com"}
