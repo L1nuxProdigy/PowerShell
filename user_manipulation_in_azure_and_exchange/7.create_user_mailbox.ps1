6650eee1c063764ddd943eb2fea87ccf0622b9a2
Nadav Dola <nadav@Nadavs-MacBook-Pro.local>
2020-03-24 15:09:11 +0200

    fixes

--- user_manipulation_in_azure_and_exchange/7.create_user_mailbox.ps1
+++ user_manipulation_in_azure_and_exchange/7.create_user_mailbox.ps1
@@ -1,9 +1,9 @@
 ## Creates a folder in the root folder hierarchy
 New-MailboxFolder -Parent "$FirstNameLowerCase@$DomainName.com" -Name $FolderName
 
+## Creates a move rule
+New-InboxRule -Name "" -Mailbox "$FirstNameLowerCase@$DomainName.com" -From "" -MoveToFolder :\$FolderName
+New-InboxRule -Name "" -Mailbox "$FirstNameLowerCase@$DomainName.com" -From "" -MoveToFolder :\$FolderName
+
 ## Retrieves all Inbox rules for the specific mailbox
 Get-InboxRule -Mailbox "$FirstNameLowerCase@$DomainName.com"
-
-## Creates a move rule
-New-InboxRule -Name "" -From "" -MoveToFolder $FolderName
-New-InboxRule -Name "" -From "" -MoveToFolder $FolderName
